6a7cb7cab5cbac168f5be5365208b2cb5e8d26fe
SamriekLeeuwin <semriekk@gmail.com>
2025-05-18 19:28:59 +0200

    Add fields and annotations to SignupRequest class
    Introduced `username`, `password`, `email`, and a default `role` to the `SignupRequest` class. Added `@Getter` and `@Setter` annotations for cleaner code and encapsulation. This prepares the class for handling user signup data effectively.

--- src/main/java/com/studybuddy/payload/SignupRequest.java
+++ src/main/java/com/studybuddy/payload/SignupRequest.java
@@ -1,9 +1,14 @@
 package com.studybuddy.payload;
 
+import com.studybuddy.model.Role;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupRequest {
-
-
-    public String getUsername() {
-        return Username();
-    }
+    private String username;
+    private String password;
+    private String email;
+    private Role role = Role.STUDENT; // Standaardrol
 }
